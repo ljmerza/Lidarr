Qstick/Sonarr

New: Custom Formats
Co-Authored-By: ta264 <ta264@users.noreply.github.com>

src/NzbDrone.Core.Test/DecisionEngineTests/UpgradeDiskSpecificationFixture.cs
@@ -4,6 +4,7 @@ using FizzWare.NBuilder;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.DecisionEngine.Specifications;
 using NzbDrone.Core.Languages;
@@ -12,6 +13,7 @@ using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Profiles.Languages;
 using NzbDrone.Core.Profiles.Qualities;
 using NzbDrone.Core.Qualities;
+using NzbDrone.Core.Test.CustomFormats;
 using NzbDrone.Core.Test.Framework;
 using NzbDrone.Core.Tv;
 
@@ -34,6 +36,8 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
             Mocker.Resolve<UpgradableSpecification>();
             _upgradeDisk = Mocker.Resolve<UpgradeDiskSpecification>();
 
+            CustomFormatsFixture.GivenCustomFormats();
+
             _firstFile = new EpisodeFile { Quality = new QualityModel(Quality.Bluray1080p, new Revision(version: 2)), DateAdded = DateTime.Now, Language = Language.English };
             _secondFile = new EpisodeFile { Quality = new QualityModel(Quality.Bluray1080p, new Revision(version: 2)), DateAdded = DateTime.Now, Language = Language.English };
 
@@ -47,7 +51,9 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
                 {
                     UpgradeAllowed = true,
                     Cutoff = Quality.Bluray1080p.Id,
-                    Items = Qualities.QualityFixture.GetDefaultQualities()
+                    Items = Qualities.QualityFixture.GetDefaultQualities(),
+                    FormatItems = CustomFormatsFixture.GetSampleFormatItems("None"),
+                    MinFormatScore = 0,
                 })
                 .With(l => l.LanguageProfile = new LanguageProfile
                 {
@@ -61,15 +67,21 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
             {
                 Series = fakeSeries,
                 ParsedEpisodeInfo = new ParsedEpisodeInfo { Quality = new QualityModel(Quality.DVD, new Revision(version: 2)), Language = Language.English },
-                Episodes = doubleEpisodeList
+                Episodes = doubleEpisodeList,
+                CustomFormats = new List<CustomFormat>()
             };
 
             _parseResultSingle = new RemoteEpisode
             {
                 Series = fakeSeries,
                 ParsedEpisodeInfo = new ParsedEpisodeInfo { Quality = new QualityModel(Quality.DVD, new Revision(version: 2)), Language = Language.English },
-                Episodes = singleEpisodeList
+                Episodes = singleEpisodeList,
+                CustomFormats = new List<CustomFormat>()
             };
+
+            Mocker.GetMock<ICustomFormatCalculationService>()
+                  .Setup(x => x.ParseCustomFormat(It.IsAny<EpisodeFile>()))
+                  .Returns(new List<CustomFormat>());
         }
 
         private void WithFirstFileUpgradable()
@@ -143,11 +155,11 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
         [Test]
         public void should_not_be_upgradable_if_revision_downgrade_and_preferred_word_upgrade_if_propers_are_preferred()
         {
-            Mocker.GetMock<IEpisodeFilePreferredWordCalculator>()
-                  .Setup(s => s.Calculate(It.IsAny<Series>(), It.IsAny<EpisodeFile>()))
-                  .Returns(5);
+            Mocker.GetMock<ICustomFormatCalculationService>()
+                  .Setup(s => s.ParseCustomFormat(It.IsAny<EpisodeFile>()))
+                  .Returns(new List<CustomFormat>());
 
-            _parseResultSingle.PreferredWordScore = 10;
+            _parseResultSingle.CustomFormatScore = 10;
 
             _firstFile.Quality = new QualityModel(Quality.WEBDL1080p, new Revision(2));
             _parseResultSingle.ParsedEpisodeInfo.Quality = new QualityModel(Quality.WEBDL1080p);

src/NzbDrone.Core/CustomFormats/Specifications/CustomFormatSpecificationBase.cs
@@ -1,0 +1,34 @@
+using NzbDrone.Core.Parser.Model;
+
+namespace NzbDrone.Core.CustomFormats
+{
+    public abstract class CustomFormatSpecificationBase : ICustomFormatSpecification
+    {
+        public abstract int Order { get; }
+        public abstract string ImplementationName { get; }
+
+        public virtual string InfoLink => "https://wiki.servarr.com/sonarr/settings#custom-formats-2";
+
+        public string Name { get; set; }
+        public bool Negate { get; set; }
+        public bool Required { get; set; }
+
+        public ICustomFormatSpecification Clone()
+        {
+            return (ICustomFormatSpecification)MemberwiseClone();
+        }
+
+        public bool IsSatisfiedBy(ParsedEpisodeInfo episodeInfo)
+        {
+            var match = IsSatisfiedByWithoutNegate(episodeInfo);
+            if (Negate)
+            {
+                match = !match;
+            }
+
+            return match;
+        }
+
+        protected abstract bool IsSatisfiedByWithoutNegate(ParsedEpisodeInfo episodeInfo);
+    }
+}

src/NzbDrone.Core/DecisionEngine/DownloadDecisionMaker.cs
@@ -5,6 +5,7 @@ using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Common.Serializer;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.DataAugmentation.Scene;
 using NzbDrone.Core.DecisionEngine.Specifications;
 using NzbDrone.Core.Download.Aggregation;
@@ -24,18 +25,21 @@ namespace NzbDrone.Core.DecisionEngine
     {
         private readonly IEnumerable<IDecisionEngineSpecification> _specifications;
         private readonly IParsingService _parsingService;
+        private readonly ICustomFormatCalculationService _formatCalculator;
         private readonly IRemoteEpisodeAggregationService _aggregationService;
         private readonly ISceneMappingService _sceneMappingService;
         private readonly Logger _logger;
 
         public DownloadDecisionMaker(IEnumerable<IDecisionEngineSpecification> specifications,
                                      IParsingService parsingService,
+                                     ICustomFormatCalculationService formatService,
                                      IRemoteEpisodeAggregationService aggregationService,
                                      ISceneMappingService sceneMappingService,
                                      Logger logger)
         {
             _specifications = specifications;
             _parsingService = parsingService;
+            _formatCalculator = formatService;
             _aggregationService = aggregationService;
             _sceneMappingService = sceneMappingService;
             _logger = logger;
@@ -89,6 +93,9 @@ namespace NzbDrone.Core.DecisionEngine
                         var remoteEpisode = _parsingService.Map(parsedEpisodeInfo, report.TvdbId, report.TvRageId, searchCriteria);
                         remoteEpisode.Release = report;
 
+                        remoteEpisode.CustomFormats = _formatCalculator.ParseCustomFormat(parsedEpisodeInfo);
+                        remoteEpisode.CustomFormatScore = remoteEpisode?.Series?.QualityProfile?.Value.CalculateCustomFormatScore(remoteEpisode.CustomFormats) ?? 0;
+
                         if (remoteEpisode.Series == null)
                         {
                             var reason = "Unknown Series";
@@ -129,7 +136,7 @@ namespace NzbDrone.Core.DecisionEngine
                             var remoteEpisode = new RemoteEpisode
                             {
                                 Release = report,
-                                ParsedEpisodeInfo = parsedEpisodeInfo
+                                ParsedEpisodeInfo = parsedEpisodeInfo,
                             };
 
                             decision = new DownloadDecision(remoteEpisode, new Rejection("Unable to parse release"));

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -2,6 +2,7 @@ using System;
 using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.History;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
@@ -13,20 +14,20 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
     {
         private readonly IHistoryService _historyService;
         private readonly UpgradableSpecification _upgradableSpecification;
+        private readonly ICustomFormatCalculationService _formatService;
         private readonly IConfigService _configService;
-        private readonly IPreferredWordService _preferredWordServiceCalculator;
         private readonly Logger _logger;
 
         public HistorySpecification(IHistoryService historyService,
-                                           UpgradableSpecification upgradableSpecification,
-                                           IConfigService configService,
-                                           IPreferredWordService preferredWordServiceCalculator,
-                                           Logger logger)
+                                    UpgradableSpecification upgradableSpecification,
+                                    ICustomFormatCalculationService formatService,
+                                    IConfigService configService,
+                                    Logger logger)
         {
             _historyService = historyService;
             _upgradableSpecification = upgradableSpecification;
+            _formatService = formatService;
             _configService = configService;
-            _preferredWordServiceCalculator = preferredWordServiceCalculator;
             _logger = logger;
         }
 
@@ -58,28 +59,27 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
                         continue;
                     }
 
+                    var customFormats = _formatService.ParseCustomFormat(mostRecent);
+
                     // The series will be the same as the one in history since it's the same episode.
                     // Instead of fetching the series from the DB reuse the known series.
-                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Series, mostRecent.SourceTitle, subject.Release?.IndexerId ?? 0);
-
                     var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
                         subject.Series.QualityProfile,
                         subject.Series.LanguageProfile,
                         mostRecent.Quality,
                         mostRecent.Language,
-                        preferredWordScore,
-                        subject.ParsedEpisodeInfo.Quality,
-                        subject.PreferredWordScore);
+                        customFormats,
+                        subject.ParsedEpisodeInfo.Quality);
 
                     var upgradeable = _upgradableSpecification.IsUpgradable(
                         subject.Series.QualityProfile,
                         subject.Series.LanguageProfile,
                         mostRecent.Quality,
                         mostRecent.Language,
-                        preferredWordScore,
+                        customFormats,
                         subject.ParsedEpisodeInfo.Quality,
                         subject.ParsedEpisodeInfo.Language,
-                        subject.PreferredWordScore);
+                        subject.CustomFormats);
 
                     if (!cutoffUnmet)
                     {

ljmerza/Lidarr

src/NzbDrone.Core.Test/DecisionEngineTests/UpgradeDiskSpecificationFixture.cs
@@ -4,12 +4,15 @@ using FizzWare.NBuilder;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
+using NzbDrone.Core.CustomFormats;
+using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.DecisionEngine.Specifications;
 using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.Music;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Profiles.Qualities;
 using NzbDrone.Core.Qualities;
+using NzbDrone.Core.Test.CustomFormats;
 using NzbDrone.Core.Test.Framework;
 
 namespace NzbDrone.Core.Test.DecisionEngineTests
@@ -28,6 +31,8 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
         {
             Mocker.Resolve<UpgradableSpecification>();
 
+            CustomFormatsFixture.GivenCustomFormats();
+
             _firstFile = new TrackFile { Quality = new QualityModel(Quality.FLAC, new Revision(version: 2)), DateAdded = DateTime.Now };
             _secondFile = new TrackFile { Quality = new QualityModel(Quality.FLAC, new Revision(version: 2)), DateAdded = DateTime.Now };
 
@@ -39,7 +44,9 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
                          {
                              UpgradeAllowed = true,
                              Cutoff = Quality.MP3_320.Id,
-                             Items = Qualities.QualityFixture.GetDefaultQualities()
+                             Items = Qualities.QualityFixture.GetDefaultQualities(),
+                             FormatItems = CustomFormatsFixture.GetSampleFormatItems("None"),
+                             MinFormatScore = 0,
                          })
                          .Build();
 
@@ -55,15 +62,21 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
             {
                 Artist = fakeArtist,
                 ParsedAlbumInfo = new ParsedAlbumInfo { Quality = new QualityModel(Quality.MP3_256, new Revision(version: 2)) },
-                Albums = doubleAlbumList
+                Albums = doubleAlbumList,
+                CustomFormats = new List<CustomFormat>()
             };
 
             _parseResultSingle = new RemoteAlbum
             {
                 Artist = fakeArtist,
                 ParsedAlbumInfo = new ParsedAlbumInfo { Quality = new QualityModel(Quality.MP3_256, new Revision(version: 2)) },
-                Albums = singleAlbumList
+                Albums = singleAlbumList,
+                CustomFormats = new List<CustomFormat>()
             };
+
+            Mocker.GetMock<ICustomFormatCalculationService>()
+                  .Setup(x => x.ParseCustomFormat(It.IsAny<TrackFile>()))
+                  .Returns(new List<CustomFormat>());
         }
 
         private void WithFirstFileUpgradable()
@@ -148,6 +161,10 @@ namespace NzbDrone.Core.Test.DecisionEngineTests
         [Test]
         public void should_be_false_if_some_tracks_are_upgradable_and_some_are_downgrades()
         {
+            Mocker.GetMock<ICustomFormatCalculationService>()
+                  .Setup(s => s.ParseCustomFormat(It.IsAny<TrackFile>()))
+                  .Returns(new List<CustomFormat>());
+
             WithFirstFileUpgradable();
             _parseResultSingle.ParsedAlbumInfo.Quality = new QualityModel(Quality.MP3_320);
             Subject.IsSatisfiedBy(_parseResultSingle, null).Accepted.Should().BeFalse();

src/NzbDrone.Core/CustomFormats/Specifications/CustomFormatSpecificationBase.cs
@@ -1,0 +1,37 @@
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.CustomFormats
+{
+    public abstract class CustomFormatSpecificationBase : ICustomFormatSpecification
+    {
+        public abstract int Order { get; }
+        public abstract string ImplementationName { get; }
+
+        public virtual string InfoLink => "https://wiki.servarr.com/sonarr/settings#custom-formats-2";
+
+        public string Name { get; set; }
+        public bool Negate { get; set; }
+        public bool Required { get; set; }
+
+        public ICustomFormatSpecification Clone()
+        {
+            return (ICustomFormatSpecification)MemberwiseClone();
+        }
+
+        public abstract NzbDroneValidationResult Validate();
+
+        public bool IsSatisfiedBy(ParsedAlbumInfo episodeInfo)
+        {
+            var match = IsSatisfiedByWithoutNegate(episodeInfo);
+            if (Negate)
+            {
+                match = !match;
+            }
+
+            return match;
+        }
+
+        protected abstract bool IsSatisfiedByWithoutNegate(ParsedAlbumInfo episodeInfo);
+    }
+}

src/NzbDrone.Core/DecisionEngine/DownloadDecisionMaker.cs
@@ -5,6 +5,7 @@ using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Common.Serializer;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.DecisionEngine.Specifications;
 using NzbDrone.Core.Download.Aggregation;
 using NzbDrone.Core.IndexerSearch.Definitions;
@@ -22,17 +23,20 @@ namespace NzbDrone.Core.DecisionEngine
     public class DownloadDecisionMaker : IMakeDownloadDecision
     {
         private readonly IEnumerable<IDecisionEngineSpecification> _specifications;
+        private readonly ICustomFormatCalculationService _formatCalculator;
         private readonly IParsingService _parsingService;
         private readonly IRemoteAlbumAggregationService _aggregationService;
         private readonly Logger _logger;
 
         public DownloadDecisionMaker(IEnumerable<IDecisionEngineSpecification> specifications,
             IParsingService parsingService,
+            ICustomFormatCalculationService formatService,
             IRemoteAlbumAggregationService aggregationService,
             Logger logger)
         {
             _specifications = specifications;
             _parsingService = parsingService;
+            _formatCalculator = formatService;
             _aggregationService = aggregationService;
             _logger = logger;
         }
@@ -96,6 +100,9 @@ namespace NzbDrone.Core.DecisionEngine
                         if (!parsedAlbumInfo.ArtistName.IsNullOrWhiteSpace())
                         {
                             var remoteAlbum = _parsingService.Map(parsedAlbumInfo, searchCriteria);
+
+                            remoteAlbum.CustomFormats = _formatCalculator.ParseCustomFormat(parsedAlbumInfo);
+                            remoteAlbum.CustomFormatScore = remoteAlbum?.Artist?.QualityProfile?.Value.CalculateCustomFormatScore(remoteAlbum.CustomFormats) ?? 0;
 
                             // try parsing again using the search criteria, in case it parsed but parsed incorrectly
                             if ((remoteAlbum.Artist == null || remoteAlbum.Albums.Empty()) && searchCriteria != null)

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NLog;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.History;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
@@ -15,20 +16,20 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
     {
         private readonly IHistoryService _historyService;
         private readonly UpgradableSpecification _upgradableSpecification;
+        private readonly ICustomFormatCalculationService _formatService;
         private readonly IConfigService _configService;
-        private readonly IPreferredWordService _preferredWordServiceCalculator;
         private readonly Logger _logger;
 
         public HistorySpecification(IHistoryService historyService,
-                                           UpgradableSpecification qualityUpgradableSpecification,
-                                           IConfigService configService,
-                                           IPreferredWordService preferredWordServiceCalculator,
-                                           Logger logger)
+                                    UpgradableSpecification upgradableSpecification,
+                                    ICustomFormatCalculationService formatService,
+                                    IConfigService configService,
+                                    Logger logger)
         {
             _historyService = historyService;
-            _upgradableSpecification = qualityUpgradableSpecification;
+            _upgradableSpecification = upgradableSpecification;
+            _formatService = formatService;
             _configService = configService;
-            _preferredWordServiceCalculator = preferredWordServiceCalculator;
             _logger = logger;
         }
 
@@ -60,23 +61,22 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
                         continue;
                     }
 
-                    // The artist will be the same as the one in history since it's the same album.
-                    // Instead of fetching the artist from the DB reuse the known artist.
-                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Artist, mostRecent.SourceTitle, subject.Release?.IndexerId ?? 0);
+                    var customFormats = _formatService.ParseCustomFormat(mostRecent);
 
+                    // The series will be the same as the one in history since it's the same episode.
+                    // Instead of fetching the series from the DB reuse the known series.
                     var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
                         subject.Artist.QualityProfile,
                         new List<QualityModel> { mostRecent.Quality },
-                        preferredWordScore,
-                        subject.ParsedAlbumInfo.Quality,
-                        subject.PreferredWordScore);
+                        customFormats,
+                        subject.ParsedAlbumInfo.Quality);
 
                     var upgradeable = _upgradableSpecification.IsUpgradable(
                         subject.Artist.QualityProfile,
                         new List<QualityModel> { mostRecent.Quality },
-                        preferredWordScore,
+                        customFormats,
                         subject.ParsedAlbumInfo.Quality,
-                        subject.PreferredWordScore);
+                        subject.CustomFormats);
 
                     if (!cutoffUnmet)
                     {